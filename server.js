21d62c955166ee2dc58afc407c262c49f389eea5
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-09-22 14:36:46 +0545

    imported CSS

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const path = require("path");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join("assets")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/getNavbar", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/components/Navbar.html"));
 });
 
 app.listen(PORT);
