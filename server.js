5bff6fdc66ede4097fb9f847d2990e4d39924a41
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-09-22 12:58:41 +0545

    Project Setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT);
+console.log("Listening on port: " + PORT);
